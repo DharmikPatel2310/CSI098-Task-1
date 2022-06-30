f704fcfe1e840a6a9129754de64c161a2a4548de
DharmikPatel2310 <72548246+DharmikPatel2310@users.noreply.github.com>
2022-06-30 23:27:18 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const connectDB = async() => {
+    try {
+        console.log(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI
+            // useNewUrlParser: true,
+            // useUnifiedTopology: true,
+            // useCreateIndex: true,
+            // useFindAndModify: false,
+        )
+        console.log('MongoDB connected.')
+    } catch (error) {
+        console.error(`Error:${error.message}`)
+        process.exit(1)
+    }
+}
+
+module.exports = connectDB;
+// export default connectDB
